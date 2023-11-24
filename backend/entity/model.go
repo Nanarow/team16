bff1291d74f84b9526b919aa3845d13b9b5bc4c3
waranyafeen <waranya02012546@gmaiil.com>
2023-11-24 16:21:21 +0700

    update

--- backend/entity/model.go
+++ backend/entity/model.go
@@ -1,5 +1,7 @@
 package entity
 
+import "time"
+
 type User struct {
 	BaseModel
 	FirstName string
@@ -13,4 +15,38 @@ type User struct {
 type LoginPayload struct {
 	Email    string
 	Password string
+}
+
+type Horse struct {
+	BaseModel
+	Name       string
+	Age        string
+	Date       time.Time
+	Image      string
+	EmployeeID uint
+	BreedId    uint
+	SexID      uint
+	StableID   uint
+}
+
+type Stable struct {
+	BaseModel
+	Maintenance time.Time
+	Cleaning    time.Time
+	Temperature string
+	Humidity    string
+	Horses      []Horse
+}
+
+type Bleed struct {
+	BaseModel
+	Name        string
+	Description string
+	Horses      []Horse
+}
+
+type Sex struct {
+	BaseModel
+	Name   string
+	Horses []Horse
 }
