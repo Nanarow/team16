87430beb9b313dcd760767719bba281a41cf5446
Woranan Sholee <woranan.sholee@gmail.com>
2023-11-24 16:14:19 +0700

    update

--- backend/entity/model.go
+++ backend/entity/model.go
@@ -1,16 +1,45 @@
 package entity
 
+import "time"
+
 type User struct {
 	BaseModel
-	FirstName string
-	LastName  string
-	Email     string
-	Password  string
-	Phone     string
-	Role      string
+	FirstName        string
+	LastName         string
+	Email            string
+	Password         string
+	Phone            string
+	Role             string
+	TourRegistration []TourRegistration
+	Enrollment       []Enrollment
 }
 
 type LoginPayload struct {
 	Email    string
 	Password string
+}
+
+type TourType struct {
+	BaseModel
+	Name           string
+	MinParticipant int
+	MaxParticipant int
+	Description    string
+}
+
+type TourRegistration struct {
+	BaseModel
+	UserID     uint
+	TourTypeID uint
+	Schedule   uint
+	Name       string
+	Date       time.Time
+}
+
+type Enrollment struct {
+	BaseModel
+	UserID   uint
+	CourseID uint
+	Date     time.Time
+	Remark   string
 }
