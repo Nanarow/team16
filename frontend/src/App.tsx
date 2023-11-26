8a62d03217d0878d85c1fcb02ce8a1289ba2c683
Woranan Sholee <woranan.sholee@gmail.com>
2023-11-26 16:18:02 +0700

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@shadcn/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen bg-slate-400 flex items-center justify-center">
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
